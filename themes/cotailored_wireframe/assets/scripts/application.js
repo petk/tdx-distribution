3da5f4f81e051621b07f59177cd48f4137f462a1
Mitja Krope <mitja.krope@agiledrop.com>
2018-03-28 12:18:20 +0200

    Install config updates for media modules, theme corrections for wireframe and base theme

--- themes/cotailored_wireframe/assets/scripts/application.js
+++ themes/cotailored_wireframe/assets/scripts/application.js
@@ -32,31 +32,6 @@
     }
   };
 
-  // Vieport
-  Drupal.behaviors.viewPort = {
-    attach: function (context) {
-      var $box = $('.boxed');
-
-      // Helper function to check if element is in viewport
-      function isInViewport(element) {
-        $(element).each(function () {
-          var $this = $(this),
-            wHeight = $(window).height(),
-            box = $this[0].getBoundingClientRect();
-            if (box.top >= 0 && box.bottom <= wHeight) {
-              $(this).addClass('animated fadeIn');
-            }
-        });
-      }
-
-      $(document).ready(function() {
-        $(document).on('scroll', function() {
-          isInViewport($box);
-        }).trigger('scroll');
-      });
-    }
-  };
-
 })(jQuery);
 
 
